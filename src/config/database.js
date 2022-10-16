2192a795500a69c06e1a69c775de5b54c14658fc
IvairSena <juniorsena866@gmail.com>
2022-10-16 14:26:40 -0300

    adding project

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'root',
+    password: 'null',
+    database: 'biblioteca',
+    host: '172.27.0.1',
+    define: { 
+        timestamps: true,
+        underscroe: true,
+    },
+};
+
